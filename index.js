18f7341932c76f0234a836c9def3d8c063b484ae
yotatoptop <141916539+yotatoptop@users.noreply.github.com>
2023-08-10 14:00:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const steamUser = require('steam-user');
+const steamTotp = require('steam-totp');
+const keep_alive = require('./keep_alive.js')
+
+var username = 'o_0frag';
+var password = 'ProNoob4ik18s';
+var shared_secret = '';
+
+var games = [570];
+var status = 1;
+
+
+user = new steamUser();
+user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
+user.on('loggedOn', () => {
+	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
+	user.setPersona(status);               
+	user.gamesPlayed(games);
+});
